c20b607d1c0e58b765aa8b3e3234e302922d9066
Melike Arslan <Melike.Arslan@n11.com>
2020-12-06 21:50:17 +0300

    Initial Commit

---
+++ java-graphql-backend/src/main/java/com/betterall/javagraphqlbackend/JavaGraphqlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.betterall.javagraphqlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGraphqlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGraphqlBackendApplication.class, args);
+    }
+
+}
---
+++ java-graphql-backend/src/test/java/com/betterall/javagraphqlbackend/JavaGraphqlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterall.javagraphqlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGraphqlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
