1be158f2fbab9e27752c39ae053da0534debbd5f
Melike Arslan <Melike.Arslan@n11.com>
2020-12-06 22:52:00 +0300

    Remove Google Cloud Dependency

--- java-graphql-backend/src/main/java/com/betterall/javagraphqlbackend/JavaGraphqlBackendApplication.java
+++ java-graphql-backend/src/main/java/com/betterall/javagraphqlbackend/JavaGraphqlBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaGraphqlBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaGraphqlBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGraphqlBackendApplication.class, args);
+	}
 
 }
