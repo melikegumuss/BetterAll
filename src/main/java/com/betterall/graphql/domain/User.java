9613282115090d6f47cf33d8fecc63a0daf1d0e0
Melike <melikearslan97@hotmail.com>
2021-01-31 18:32:04 +0300

    Local Database connection and user table name changed

--- src/main/java/com/betterall/graphql/domain/User.java
+++ src/main/java/com/betterall/graphql/domain/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name="user")
+@Table(name="registered_user")
 @Getter
 @Setter
 public class User implements Serializable {
