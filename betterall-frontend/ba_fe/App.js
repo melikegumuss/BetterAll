3aaeeefeadbacc95f19840cbcb4b99796e6786d6
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berk.buyukdurak@ug.bilkent.edu.tr>
2021-02-25 14:23:19 +0300

    FE - Setting development environment is completed. Initial project for the front-end development is created.

---
+++ betterall-frontend/ba_fe/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+
+//#region Public class
+//#endregion
+export default class App extends Component{
+
+  //#region Program starts
+  //#endregion
+  render(){
+    return(
+      <View>
+        <Text>ALLAHIM SONUNDA OLDU</Text>
+      </View>
+    );
+  }
+}
