32d06a7cf1d8d176dde9fc9f98e6d146508c0dce
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berk.buyukdurak@ug.bilkent.edu.tr>
2021-03-07 01:13:27 +0300

    FE - Startup Screen - Initial commit of Startup Screen.

--- betterall-frontend/ba_fe/App.js
+++ betterall-frontend/ba_fe/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import styles from './Styles/style'
 //#region Public class
