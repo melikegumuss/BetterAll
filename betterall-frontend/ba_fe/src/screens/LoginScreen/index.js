770a5698b348f588fb03503af5b76ebc24ef6d8c
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2021-03-19 23:05:00 +0300

    Login Screen and Register Screen created (UI only). Register Screen lacks proper format of the components (ireplace text fields with appropriate components i.e. date picker to be added).

--- betterall-frontend/ba_fe/src/screens/LoginScreen/index.js
+++ betterall-frontend/ba_fe/src/screens/LoginScreen/index.js
@@ -1,15 +1,113 @@
-import * as React from 'react';
-import {Text, View} from 'react-native';
+//import * as React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Button } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import React, {useState} from 'react';
 
-export default class LoginScreen extends React.Component {
+
+export default class RegisterScreen extends React.Component {
   constructor() {
     super();
   }
+  state={
+    username:"",
+    email:"",
+    password:"",
+    name:"",
+    surname:"",
+    height:"",
+    weight:"",
+    gender:"",
+    //age: new Date(),
+  }
+
+
   render() {
+
     return (
-      <View>
-        <Text>LoginScreen</Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>Welcome back!</Text>
+        <ScrollView>
+          <View style={styles.inputView} >
+            <TextInput
+              style={styles.inputText}
+              placeholder="Username"
+              placeholderTextColor="rgba(0,0,0,0.35)"
+              onChangeText={text => this.setState({username:text})}/>
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              secureTextEntry
+              style={styles.inputText}
+              placeholder="Password..."
+              placeholderTextColor="rgba(0,0,0,0.35)"
+              onChangeText={text => this.setState({password:text})}/>
+          </View>
+          <TouchableOpacity style={styles.forgotPassword}>
+            <Text style={styles.forgotPassword}>Forgot password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>LOGIN</Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#CDDA7E',
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  header: {
+    fontSize: 48,
+    color:'#7B8235',
+    paddingBottom: 10,
+    marginTop: 30,
+    marginBottom: 40,
+    borderBottomColor: '#7B8235',
+    //borderBottomColor: '#199187',
+    borderBottomWidth: 1,
+  },
+  inputView:{
+    width:"100%",
+    backgroundColor:'rgba(0,0,0,0.09)',
+    borderRadius:25,
+    height:30,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20,
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  loginButton:{
+    width:"30%",
+    backgroundColor:"#ffcc33",
+    borderRadius:25,
+    height:35,
+    alignItems:"center",
+    justifyContent:"center",
+    marginLeft:240,
+    marginTop:15,
+    //marginBottom:10
+  },
+  loginButtonText:{
+    color:'rgba(0,0,0,0.6)',
+  },
+  forgotPassword:{
+    color:'rgba(0,0,0,0.6)',
+    marginLeft:120,
+  },
+  datePickerStyle: {
+    width: 200,
+    marginTop: 20,
+  },
+});
