6fd2d26d31453b4392e7fcc4868ad00ca92d1043
Ece ÇANGA <44345414+ececanga@users.noreply.github.com>
2021-04-11 15:59:43 +0300

    App menu screen added. Images for the button components are created and included as png files. Navigation is updated with the new pages. Font updated. Packages installed:  react-native-profile-picture@1.0.0  fontsource-muli@4.0.0  react-native-safe-area-context@3.2.0

---
+++ betterall-frontend/ba_fe/src/screens/ViewProgressScreen/index.js
@@ -0,0 +1,51 @@
+//import * as React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Button, ScrollView } from "react-native";
+import React, {useState} from 'react';
+
+
+export default class CalculateDailyCalorieScreen extends React.Component {
+  constructor() {
+    super();
+  }
+  state={
+    username:"",
+    email:"",
+    password:"",
+    name:"",
+    surname:"",
+    height:"",
+    weight:"",
+    gender:"",
+    //age: new Date(),
+  }
+
+
+  render() {
+    return (
+          <View style={styles.container}>
+            <Text style={styles.header}>CalculateDailyCalorieScreen</Text>
+          </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#CDDA7E',
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  header: {
+    fontSize: 48,
+    fontFamily:'Mulish-Regular',
+    color:'#7B8235',
+    paddingBottom: 10,
+    marginTop: 30,
+    marginBottom: 40,
+    borderBottomColor: '#7B8235',
+    //borderBottomColor: '#199187',
+    borderBottomWidth: 1,
+  },
+
+});
