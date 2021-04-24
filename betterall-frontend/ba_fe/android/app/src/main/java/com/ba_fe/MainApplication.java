9f6c9a44b1fcba2f2802dbcbe634e5b4e4815b1a
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berk.buyukdurak@ug.bilkent.edu.tr>
2021-04-24 10:31:49 +0300

    BAFE-SPRINT-2 -- Google Maps Places API and its key are added.

--- betterall-frontend/ba_fe/android/app/src/main/java/com/ba_fe/MainApplication.java
+++ betterall-frontend/ba_fe/android/app/src/main/java/com/ba_fe/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          //packages.add(new RNGooglePlacesPackage());
+          //packages.add(new MapsPackage());
           return packages;
         }
 
--- betterall-frontend/ba_fe/src/screens/Home/index.js
+++ betterall-frontend/ba_fe/src/screens/Home/index.js
@@ -1,40 +1,68 @@
 /*
 * This is only for debugging purposes. Will be deleted later.
 * */
-import {View, Button, StyleSheet, Dimensions} from 'react-native';
+import {View, Button, StyleSheet, Dimensions, TouchableOpacity, Text} from 'react-native';
 import React from 'react';
 import {Component} from 'react';
 import RegisterScreen from '../RegisterScreen';
 import "../../../assets/fonts/Mulish-Regular.ttf";
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import {locations} from '../data/data';
+import RNGooglePlaces from 'react-native-google-places';
+import Geolocation from '@react-native-community/geolocation';
+//import PlaceList from "../Home/PlaceList";
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: Dimensions.height,
-    width: Dimensions.width,
-    //justifyContent: 'flex-end',
-    //alignItems: 'center',
+    //height: 400,
+    //width: 250,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    //...StyleSheet.absoluteFillObject,
+    height: 600,
+    width: 400,
   },
 });
 
 export default class Home extends Component {
-  constructor() {
-    super();
+
+  getCurrentLocation(){
+    Geolocation.getCurrentPosition(info => console.log(info),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
   }
+
+  openSearchModal() {
+    RNGooglePlaces.openAutocompleteModal()
+    .then((place) => {
+		console.log(place);
+		// place represents user's selection from the
+		// suggestions and it is a simplified Google Place object.
+    })
+    .catch(error => console.log(error.message),
+    );  // error is a Javascript Error object
+  }
+
   render() {
     return (
       <View style={styles.container}>
+
+      <TouchableOpacity
+        //style={styles.button}
+        onPress={() => this.getCurrentLocation()}
+      >
+        <Text style = {{marginTop:5,padding: 30}} >Pick a Place</Text>
+      </TouchableOpacity>
+
+
       <MapView
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={styles.map}
         region={{
-          latitude: 24.83073230,
-          longitude: 67.10113298,
+          latitude: -14.29501,
+          longitude:  23.44455,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
