8090b425d8cf474d56b8001561b9bb4f4a307fc4
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berk.buyukdurak@ug.bilkent.edu.tr>
2021-04-24 16:09:33 +0300

    BAFE-SPRINT-2 -- Current Location Detection via Google Maps is debugged. Close to the final. npm install @react-native-community/geolocation --save

--- betterall-frontend/ba_fe/src/screens/Home/index.js
+++ betterall-frontend/ba_fe/src/screens/Home/index.js
@@ -10,8 +10,14 @@ import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import {locations} from '../data/data';
 import RNGooglePlaces from 'react-native-google-places';
 import Geolocation from '@react-native-community/geolocation';
-//import PlaceList from "../Home/PlaceList";
 
+const {width, height} = Dimensions.get('window')
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+/*
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
@@ -25,10 +31,74 @@ const styles = StyleSheet.create({
     height: 600,
     width: 400,
   },
+});*/
+const styles = StyleSheet.create({
+  container: {
+    /*
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',*/
+    position: 'absolute',
+    height : SCREEN_HEIGHT,
+    width : SCREEN_WIDTH,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 });
 
 export default class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+    }
+  }
 
+  componentDidMount() {
+    Geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+
+      var initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+
+      this.setState({initialPosition: initialRegion})
+    },
+    (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000});
+  }
+
+  renderScreen = () => {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={this.state.initialPosition}/>
+      </View>
+    );
+}
+
+  /*
   getCurrentLocation(){
     Geolocation.getCurrentPosition(info => console.log(info),
     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
@@ -43,11 +113,12 @@ export default class Home extends Component {
     })
     .catch(error => console.log(error.message),
     );  // error is a Javascript Error object
-  }
+  }*/
 
   render() {
     return (
-      <View style={styles.container}>
+      this.renderScreen()
+      /*<View style={styles.container}>
 
       <TouchableOpacity
         //style={styles.button}
@@ -77,7 +148,7 @@ export default class Home extends Component {
           ))
         }
       </MapView>
-    </View>
+    </View>*/
     );
   }
 }
