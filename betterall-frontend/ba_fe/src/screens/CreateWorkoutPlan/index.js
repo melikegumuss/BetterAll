08ca1c5e99bf72b28034865e956d8e3077cfbec8
İlknur Baş <ilknurbas67@gmail.com>
2021-04-28 12:43:50 +0300

    Modal is added to "CreateWorkoutPlan" screen.

--- betterall-frontend/ba_fe/src/screens/CreateWorkoutPlan/index.js
+++ betterall-frontend/ba_fe/src/screens/CreateWorkoutPlan/index.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Alert, Modal, ImageBackground} from 'react-native';
 import GainWeight from '../../../assets/images/gainWeight.png';
 import GetFit from '../../../assets/images/getFit.png';
 import GetStronger from '../../../assets/images/getStronger.png';
 import LoseWeight from '../../../assets/images/loseWeight.png';
-
 import Slider from "react-native-smooth-slider";
-
 
 import {
   SafeAreaView,
@@ -16,186 +14,196 @@ import {
   ScrollView,
 } from 'react-native';
 import Tags from "react-native-tags";
-//import Modal from "react-native-modal";
-
-
 
 export default class CreateWorkoutPlan extends Component {
   state = {
     goal: '',
     text: '',
     value: 0.2,
-    isModalVisible:false,
+    modalVisible: false,
   };
 
   constructor() {
     super();
   }
-/*  toggleModal = () => {
-    this.setState({
-      isModalVisible: !this.state.isModalVisible,
-
-    });
-    console.log(' lütfen ' + this.state.isModalVisible)
-  };*/
+  setModalVisible = (visible) => {
+    this.setState({ modalVisible: visible });
+  }
 
   render() {
+    const { modalVisible } = this.state;
     return (
       <View style={styles.container}>
         <SafeAreaView>
           <Text style={{marginTop: 20,marginRight: 20,marginLeft: 20}}>List of Workout Plans</Text>
+          <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                Alert.alert("You meal plan has not been created.");
+                this.setModalVisible(!modalVisible);
+              }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
 
-          <View  style={{alignContent:"center",marginTop: 20}}>
-{/*            <TouchableOpacity onPress={this.toggleModal} style={styles.forModal}>
-              <Text> I want to create a new workout plan</Text>
-            </TouchableOpacity>*/}
-          </View>
-
-         {/* <Modal isVisible={this.state.isModalVisible}>*/}
-            <ScrollView>
-            <Text style={styles.titleStyle}>
-              Weakest muscle group
-            </Text>
-
-            <ScrollView horizontal={true}>
-              <Tags createTagOnReturn
-                    initialText="monkey"
-                    textInputProps={{
-                      placeholder: "Any type of animal"
-                    }}
-                    initialTags={["dog", "cat", "chicken"]}
-                    onChangeTags={tags => console.log(tags)}
-                    onTagPress={(index, tagLabel, event, deleted) =>
-                      console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
-                    }
-                //createTagOnString={["\n"]}
-                    containerStyle={{ justifyContent: "center" }}
-                    inputStyle={{ backgroundColor: '#eceece', borderRadius: 100, color: '#9cb3d3'}}
-                    renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
-                      <TouchableOpacity key={`${tag}-${index}`} onPress={onPress} style={styles.tagTouchable}>
-                        <Text style={styles.tagTextStyle}>💪 {tag}</Text>
-                      </TouchableOpacity>
-                    )}
-              />
-            </ScrollView>
-            <Text style={styles.titleStyle}>
-              Strongest muscle group
-            </Text>
-            <ScrollView horizontal={true}>
-              <Tags createTagOnReturn
-                    initialText="monkey"
-                    textInputProps={{
-                      placeholder: "Any type of animal"
-                    }}
-                    initialTags={["dog", "cat", "chicken"]}
-                    onChangeTags={tags => console.log(tags)}
-                    onTagPress={(index, tagLabel, event, deleted) =>
-                      console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
-                    }
-                //createTagOnString={["\n"]}
-                    containerStyle={{ justifyContent: "center" }}
-                    inputStyle={{ backgroundColor: '#eceece', borderRadius: 100, color: '#9cb3d3'}}
-                    renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
-                      <TouchableOpacity key={`${tag}-${index}`} onPress={onPress} style={styles.tagTouchable}>
-                        <Text style={styles.tagTextStyle}>💪 {tag}</Text>
-                      </TouchableOpacity>
-                    )}
-              />
-            </ScrollView>
-            <Text style={styles.titleStyle}>
-              Injuries
-            </Text>
-            <ScrollView horizontal={true}>
-              <Tags createTagOnReturn
-                    initialText="monkey"
-                    textInputProps={{
-                      placeholder: "Any type of animal"
-                    }}
-                    initialTags={["dog", "cat", "chicken"]}
-                    onChangeTags={tags => console.log(tags)}
-                    onTagPress={(index, tagLabel, event, deleted) =>
-                      console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
-                    }
-                //createTagOnString={["\n"]}
-                    containerStyle={{ justifyContent: "center" }}
-                    inputStyle={{ backgroundColor: '#eceece', borderRadius: 100, color: '#9cb3d3'}}
-                    renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
-                      <TouchableOpacity key={`${tag}-${index}`} onPress={onPress} style={styles.tagTouchable}>
-                        <Text style={styles.tagTextStyle}>🤕 {tag}</Text>
-                      </TouchableOpacity>
-                    )}
-              />
-            </ScrollView>
-              <Text style={styles.titleStyle}>
-                Fitness Level: {' '}
-                  {Math.floor((this.state.value*10)/2)}
-              </Text>
-
-              <Slider
-                thumbTintColor={'#9cb3d3'}
-                minimumTrackTintColor={'#9cb3d3'}
-                step={0.2}
-                //moveVelocityThreshold={0.1}
-                thumbStyle={styles.thumb}
-                trackStyle={styles.track}
-                value={this.state.value}
-                useNativeDriver={true}
-                onValueChange={value => {
-                  clearTimeout(this.sliderTimeoutId)
-                  this.sliderTimeoutId = setTimeout(() => {
-                    this.setState({value})
-                  }, 100)
-                }}
-                // Slider moves too fast with this value of onValueChange
-                //onValueChange={value => this.setState({ value })}
-              />
-
-              <View style={styles.column}>
+                <ScrollView>
                 <Text style={styles.titleStyle}>
-                  Diet Goal
+                  Weakest muscle group
                 </Text>
-                <View style={styles.row}>
-                  <TouchableOpacity
-                    style={styles.buttonStyle}
-                    activeOpacity={0.5}>
-                    <View>
-                      <Image source={GainWeight} style={styles.image} />
-                    </View>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={styles.buttonStyle}
-                    activeOpacity={0.5}
-                    onPress={this.onPress}>
-                    <View>
-                      <Image source={LoseWeight} style={styles.image} />
-                    </View>
-                  </TouchableOpacity>
-                </View>
 
-                <View style={styles.row}>
-                  <TouchableOpacity
-                    style={styles.buttonStyle}
-                    activeOpacity={0.5}>
-                    <View>
-                      <Image source={GetFit} style={styles.image} />
+                <ScrollView horizontal={true}>
+                  <Tags createTagOnReturn
+                        initialText="monkey"
+                        textInputProps={{
+                          placeholder: "Any type of animal"
+                        }}
+                        initialTags={["dog", "cat", "chicken"]}
+                        onChangeTags={tags => console.log(tags)}
+                        onTagPress={(index, tagLabel, event, deleted) =>
+                          console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
+                        }
+                    //createTagOnString={["\n"]}
+                        containerStyle={{ justifyContent: "center" }}
+                        inputStyle={{ backgroundColor: '#eceece', borderRadius: 100, color: '#9cb3d3'}}
+                        renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
+                          <TouchableOpacity key={`${tag}-${index}`} onPress={onPress} style={styles.tagTouchable}>
+                            <Text style={styles.tagTextStyle}>💪 {tag}</Text>
+                          </TouchableOpacity>
+                        )}
+                  />
+                </ScrollView>
+                <Text style={styles.titleStyle}>
+                  Strongest muscle group
+                </Text>
+                <ScrollView horizontal={true}>
+                  <Tags createTagOnReturn
+                        initialText="monkey"
+                        textInputProps={{
+                          placeholder: "Any type of animal"
+                        }}
+                        initialTags={["dog", "cat", "chicken"]}
+                        onChangeTags={tags => console.log(tags)}
+                        onTagPress={(index, tagLabel, event, deleted) =>
+                          console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
+                        }
+                    //createTagOnString={["\n"]}
+                        containerStyle={{ justifyContent: "center" }}
+                        inputStyle={{ backgroundColor: '#eceece', borderRadius: 100, color: '#9cb3d3'}}
+                        renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
+                          <TouchableOpacity key={`${tag}-${index}`} onPress={onPress} style={styles.tagTouchable}>
+                            <Text style={styles.tagTextStyle}>💪 {tag}</Text>
+                          </TouchableOpacity>
+                        )}
+                  />
+                </ScrollView>
+                <Text style={styles.titleStyle}>
+                  Injuries
+                </Text>
+                <ScrollView horizontal={true}>
+                  <Tags createTagOnReturn
+                        initialText="monkey"
+                        textInputProps={{
+                          placeholder: "Any type of animal"
+                        }}
+                        initialTags={["dog", "cat", "chicken"]}
+                        onChangeTags={tags => console.log(tags)}
+                        onTagPress={(index, tagLabel, event, deleted) =>
+                          console.log(index, tagLabel, event, deleted ? "deleted" : "not deleted")
+                        }
+                    //createTagOnString={["\n"]}
+                        containerStyle={{ justifyContent: "center" }}
+                        inputStyle={{ backgroundColor: '#eceece', borderRadius: 100, color: '#9cb3d3'}}
+                        renderTag={({ tag, index, onPress, deleteTagOnPress, readonly }) => (
+                          <TouchableOpacity key={`${tag}-${index}`} onPress={onPress} style={styles.tagTouchable}>
+                            <Text style={styles.tagTextStyle}>🤕 {tag}</Text>
+                          </TouchableOpacity>
+                        )}
+                  />
+                </ScrollView>
+                  <Text style={styles.titleStyle}>
+                    Fitness Level: {' '}
+                      {Math.floor((this.state.value*10)/2)}
+                  </Text>
+
+                  <Slider
+                    thumbTintColor={'#9cb3d3'}
+                    minimumTrackTintColor={'#9cb3d3'}
+                    step={0.2}
+                    //moveVelocityThreshold={0.1}
+                    thumbStyle={styles.thumb}
+                    trackStyle={styles.track}
+                    value={this.state.value}
+                    useNativeDriver={true}
+                    onValueChange={value => {
+                      clearTimeout(this.sliderTimeoutId)
+                      this.sliderTimeoutId = setTimeout(() => {
+                        this.setState({value})
+                      }, 100)
+                    }}
+                    // Slider moves too fast with this value of onValueChange
+                    //onValueChange={value => this.setState({ value })}
+                  />
+
+                  <View style={styles.column}>
+                    <Text style={styles.titleStyle}>
+                      Diet Goal
+                    </Text>
+                    <View style={styles.row}>
+                      <TouchableOpacity
+                        style={styles.buttonStyle}
+                        activeOpacity={0.5}>
+                        <View>
+                          <Image source={GainWeight} style={styles.image2} />
+                        </View>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        style={styles.buttonStyle}
+                        activeOpacity={0.5}
+                        onPress={this.onPress}>
+                        <View>
+                          <Image source={LoseWeight} style={styles.image2} />
+                        </View>
+                      </TouchableOpacity>
                     </View>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={styles.buttonStyle}
-                    activeOpacity={0.5}>
-                    <View>
-                      <Image source={GetStronger} style={styles.image} />
+
+                    <View style={styles.row}>
+                      <TouchableOpacity
+                        style={styles.buttonStyle}
+                        activeOpacity={0.5}>
+                        <View>
+                          <Image source={GetFit} style={styles.image2} />
+                        </View>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        style={styles.buttonStyle}
+                        activeOpacity={0.5}>
+                        <View>
+                          <Image source={GetStronger} style={styles.image2} />
+                        </View>
+                      </TouchableOpacity>
                     </View>
-                  </TouchableOpacity>
-                </View>
+                  </View>
+
+                </ScrollView>
+
+                <TouchableOpacity
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => this.setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>GET YOUR WORKOUT PLAN!</Text>
+                </TouchableOpacity>
               </View>
-              <View  style={{alignContent:"center",marginTop: 20}}>
-{/*                <TouchableOpacity onPress={this.toggleModal} style={styles.forCreate}>
-                  <Text> Create !</Text>
-                </TouchableOpacity>*/}
-              </View>
-            </ScrollView>
-{/*         </Modal>*/}
+            </View>
+          </Modal>
+
+          <TouchableOpacity
+              style={styles.planButton}
+              onPress={() => this.setModalVisible(true)}>
+            <ImageBackground source={require("../../../assets/images/workoutPlan.png")}
+                             style={styles.image}/>
+          </TouchableOpacity>
         </SafeAreaView>
       </View>
     );
@@ -211,10 +219,10 @@ const styles = StyleSheet.create({
     //justifyContent: 'center',
     alignItems: 'stretch',
   },
-  image: {
+  image2: {
     //flex: 1,
-    width: 170,
-    height: 170,
+    width: 100,
+    height: 120,
     //resizeMode: 'contain',
     //justifyContent: 'space-evenly',
     //backgroundColor: 'black',
@@ -303,5 +311,68 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginRight: 8,
     marginBottom: 50,
+  },
+  centeredView: {
+    //flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 30,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 55,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 20,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#ffcc33",
+  },
+  buttonClose: {
+    backgroundColor: "#ffcc33",
+    width:250,
+    alignItems:'center'
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    //textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  image: {
+    flex: 1,
+    width: 150,
+    height: 150,
+    justifyContent: 'center',
+    //alignItems: 'center',
+    //resizeMode: 'contain'
+  },
+  planButton:{
+    width: 140,
+    height: 140,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //borderColor:'rgba(0,0,0,0.6)',
+    //backgroundColor: 'white',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 25,
+    marginBottom: 25,
   },
 });
