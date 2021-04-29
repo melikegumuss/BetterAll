22482c644940a94209cd13f784c4e76d71d84379
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berk.buyukdurak@ug.bilkent.edu.tr>
2021-04-29 14:57:04 +0300

    Debugging the Google Maps

--- betterall-frontend/ba_fe/src/screens/Home/index.js
+++ betterall-frontend/ba_fe/src/screens/Home/index.js
@@ -1,210 +1,123 @@
-/*
-* This is only for debugging purposes. Will be deleted later.
-* */
-import {View, Button, StyleSheet, Dimensions, TouchableOpacity, Text} from 'react-native';
-import React from 'react';
-import {Component} from 'react';
-import "../../../assets/fonts/Mulish-Regular.ttf";
-//import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import MapView, {Marker} from 'react-native-maps';
-import {locations} from '../data/data';
-import RNGooglePlaces from 'react-native-google-places';
-import Geolocation from 'react-native-geolocation-service';
-/*
-const {width, height} = Dimensions.get('window')
-const SCREEN_HEIGHT = height
-const SCREEN_WIDTH = width
-const ASPECT_RATIO = width / height
-const LATITUDE_DELTA = 0.0922
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO*/
-/*
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    //height: 400,
-    //width: 250,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    //...StyleSheet.absoluteFillObject,
-    height: 600,
-    width: 400,
-  },
-});*/
+import React, { Component } from "react";
+import {
+  View,
+  Image,
+  Button,
+  StyleSheet,
+  Text,
+  Dimensions
+} from "react-native";
+import MapView from "react-native-maps";
+import Geolocation from '@react-native-community/geolocation';
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    right: 0,
-    bottom: 0
-  }
-});
-/*
-const styles = StyleSheet.create({
-  container: {
+export default class Home extends Component {
 
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    position: 'absolute',
-    height : SCREEN_HEIGHT,
-    width : SCREEN_WIDTH,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-});*/
-type Props = {};
-export default class Home extends Component<Props> {
-  constructor(props){
-    super(props);
-    this.state = {
-      latitude: 0,
-      longitude: 0
-    }
-  }
 
-  componentDidMount(){
-    try {
-      this.watchId = Geolocation.watchPosition(
-          (position) => {
-            this.setState({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            });
-          },
-          (error) => {
-            this.setState({error: error.message})
-          },
-          {enableHighAccuracy: false, timeout: 1, maximumAge: 1, distanceFilter: 1}
-      )
-    }catch(e){
-      console.log(e);
-    }
-  }
-  /*constructor() {
-    super()
-    this.state = {
-      initialPosition: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0,
-        longitudeDelta: 0,
+  state =  {
+
+      focusedLocation: {
+        latitude: 37.7900352,
+        longitude: -122.4013726,
+        latitudeDelta: 0.0122,
+        longitudeDelta:
+          Dimensions.get("window").width /
+          Dimensions.get("window").height *
+          0.0122
       },
-    }
+      locationChosen: false
   }
-  componentDidMount() {
-    Geolocation.getCurrentPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
 
-      var initialRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      }
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    });
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        },
+        locationChosen: true
+      };
+    });
 
-      this.setState({initialPosition: initialRegion})
+  };
+
+  getLocationHandler = () => {
+    /*Geolocation.getCurrentPosition(pos => {
+      const coordsEvent = {
+        nativeEvent: {
+          coordinate: {
+            //latitude: pos.coords.latitude,
+            //longitude: pos.coords.longitude
+            latitude = JSON.stringify(pos.coords.latitude),
+            longitude = JSON.stringify(pos.coords.longitude)
+          }
+        }
+      };
+      this.pickLocationHandler(coordsEvent);
     },
-    (error) => alert(JSON.stringify(error)),
-    {enableHighAccuracy: false, timeout: 15000});
-  }*/
+  err => {
+    console.log(err);
+    alert("Fetching the Position failed, please pick one manually!");
+  }
+  )*/
+  Geolocation.getCurrentPosition(
+    //Will give you the current location
+    (position) => {
+      //getting the Longitude from the location json
+      const currentLongitude =
+        JSON.stringify(position.coords.longitude);
+  
+      //getting the Latitude from the location json
+      const currentLatitude =
+        JSON.stringify(position.coords.latitude);
+        
+     }, (error) => alert(error.message), { 
+       enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 
+     })
+  }
 
-  /*renderScreen = () => {
+  render() {
+    let marker = null;
+
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+    }
+
     return (
       <View style={styles.container}>
         <MapView
+          initialRegion={this.state.focusedLocation}
+          region={!this.state.locationChosen ? this.state.focusedLocation : null}
           style={styles.map}
-          initialRegion={this.state.initialPosition}/>
-      </View>
-    );
-}*/
-
-  /*
-  getCurrentLocation(){
-    Geolocation.getCurrentPosition(info => console.log(info),
-    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
-  }
-
-  openSearchModal() {
-    RNGooglePlaces.openAutocompleteModal()
-    .then((place) => {
-		console.log(place);
-		// place represents user's selection from the
-		// suggestions and it is a simplified Google Place object.
-    })
-    .catch(error => console.log(error.message),
-    );  // error is a Javascript Error object
-  }*/
-
-  render() {
-    let myCoordinate = { latitude: 39.9248, longitude: 32.8368}
-    let myLocation = {latitude: this.state.latitude, longitude: this.state.longitude}
-    let initialRegion = {
-      latitude: myLocation.latitude,
-      longitude: myLocation.longitude,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01
-    }
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.map} initialRegion = {initialRegion}>
-          <Marker coordinate={ myCoordinate} />
-          <Marker pinColor={'green'} coordinate={myLocation}/>
+          onPress={this.pickLocationHandler}
+          ref={ref => this.map = ref}
+        >
+          {marker}
         </MapView>
+        <View style={styles.button}>
+          <Button title="Locate Me" onPress={this.getLocationHandler} />
+        </View>
       </View>
-      //this.renderScreen()
-      /*<View style={styles.container}>
-
-      <TouchableOpacity
-        //style={styles.button}
-        onPress={() => this.getCurrentLocation()}
-      >
-        <Text style = {{marginTop:5,padding: 30}} >Pick a Place</Text>
-      </TouchableOpacity>
-
-
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: -14.29501,
-          longitude:  23.44455,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      >
-        {
-          locations.map(marker =>(
-            <Marker
-              coordinate = {{latitude: marker.latitude,
-                longitude: marker.longitude}}
-                title = {marker.title}
-            />
-          ))
-        }
-      </MapView>
-    </View>*/
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    alignItems: "center"
+  },
+  map: {
+    width: "100%",
+    height: 600
+  },
+  button: {
+    margin: 8
+  }
+});
