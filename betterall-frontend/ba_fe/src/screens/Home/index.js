6067257b7ed64b4d1193d2cc79a196a322498512
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berk.buyukdurak@ug.bilkent.edu.tr>
2021-04-29 15:31:45 +0300

    Google Maps current Location issue has resolved inş.

--- betterall-frontend/ba_fe/src/screens/Home/index.js
+++ betterall-frontend/ba_fe/src/screens/Home/index.js
@@ -1,125 +1,94 @@
-import React, { Component } from "react";
-import {
-  View,
-  Image,
-  Button,
-  StyleSheet,
-  Text,
-  Dimensions
-} from "react-native";
-import MapView from "react-native-maps";
-import Geolocation from '@react-native-community/geolocation';
-
-export default class Home extends Component {
-
-  componentDidMount(){
-    getLocationHandler()
+import React from 'react';
+import {View, Text, PermissionsAndroid, Alert, Platform} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import MapView, {PROVIDER_GOOGLE, Marker, Polyline} from 'react-native-maps';
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      coordinates: [],
+    };
   }
-  state =  {
 
-      focusedLocation: {
-        latitude: 37.7900352,
-        longitude: -122.4013726,
-        latitudeDelta: 0.0122,
-        longitudeDelta:
-          Dimensions.get("window").width /
-          Dimensions.get("window").height *
-          0.0122
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: this.state.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
       },
-      locationChosen: false
+      error => {
+        Alert.alert(error.message.toString());
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+      },
+    );
+
+    Geolocation.watchPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: this.state.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+      error => {
+        console.log(error);
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+        distanceFilter: 0,
+      },
+    );
   }
-
-  pickLocationHandler = event => {
-    const coords = event.nativeEvent.coordinate;
-    this.map.animateToRegion({
-      ...this.state.focusedLocation,
-      latitude: coords.latitude,
-      longitude: coords.longitude
-    });
-    this.setState(prevState => {
-      return {
-        focusedLocation: {
-          ...prevState.focusedLocation,
-          latitude: coords.latitude,
-          longitude: coords.longitude
-        },
-        locationChosen: true
-      };
-    });
-
-  };
-
-  getLocationHandler = () => {
-    /*Geolocation.getCurrentPosition(pos => {
-      const coordsEvent = {
-        nativeEvent: {
-          coordinate: {
-            //latitude: pos.coords.latitude,
-            //longitude: pos.coords.longitude
-            latitude = JSON.stringify(pos.coords.latitude),
-            longitude = JSON.stringify(pos.coords.longitude)
-          }
-        }
-      };
-      this.pickLocationHandler(coordsEvent);
-    },
-  err => {
-    console.log(err);
-    alert("Fetching the Position failed, please pick one manually!");
-  }
-  )*/
-  Geolocation.getCurrentPosition(
-    //Will give you the current location
-    (position) => {
-      //getting the Longitude from the location json
-      const currentLongitude =
-        JSON.stringify(position.coords.longitude);
-  
-      //getting the Latitude from the location json
-      const currentLatitude =
-        JSON.stringify(position.coords.latitude);
-        
-     }, (error) => alert(error.message), { 
-       enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 
-     })
-  }
-
   render() {
-    let marker = null;
-
-    if (this.state.locationChosen) {
-      marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
-    }
-
     return (
-      <View style={styles.container}>
+      <View style={{flex: 1}}>
         <MapView
-          initialRegion={this.state.focusedLocation}
-          region={!this.state.locationChosen ? this.state.focusedLocation : null}
-          style={styles.map}
-          onPress={this.pickLocationHandler}
-          ref={ref => this.map = ref}
-        >
-          {marker}
+          provider={PROVIDER_GOOGLE}
+          style={{flex: 1}}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: this.state.latitude,
+              longitude: this.state.longitude,
+            }}></Marker>
+          <Polyline
+            coordinates={this.state.coordinates}
+            strokeColor="#bf8221"
+            strokeColors={[
+              '#bf8221',
+              '#ffe066',
+              '#ffe066',
+              '#ffe066',
+              '#ffe066',
+            ]}
+            strokeWidth={3}
+          />
         </MapView>
-        <View style={styles.button}>
-          <Button title="Locate Me" onPress={this.getLocationHandler} />
-        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    alignItems: "center"
-  },
-  map: {
-    width: "100%",
-    height: 600
-  },
-  button: {
-    margin: 8
-  }
-});
